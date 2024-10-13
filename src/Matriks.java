19e92562fbe46aaa412d25a595ecc1c472cc0a31
MRizainF <icon.firdaus@gmail.com>
2024-10-13 08:09:53 +0700

    add determinanMatriks, hitungDeterminan, minorKofaktor, kofaktorMatriks

--- src/Matriks.java
+++ src/Matriks.java
@@ -1,4 +1,3 @@
-package src;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -71,27 +70,22 @@ public class Matriks {
             File file = new File(fileName);
             Scanner objFile = new Scanner(file);
 
-            // Read dimensions from file
             this.m_baris = 0;
             this.n_kolom = 0;
 
-            // Count rows
             while (objFile.hasNextLine()) {
                 String line = objFile.nextLine();
                 if (this.n_kolom == 0) {
-                    this.n_kolom = line.split(" ").length; // Get column count from the first line
+                    this.n_kolom = line.split(" ").length;
                 }
                 this.m_baris++;
             }
 
-            // Reset the scanner to read the file again for elements
             objFile.close();
             objFile = new Scanner(file);
 
-            // Initialize the matrix
             this.matriks = new double[this.m_baris][this.n_kolom];
 
-            // Read elements into the matrix
             for (int i = 0; i < this.m_baris; i++) {
                 for (int j = 0; j < this.n_kolom; j++) {
                     if (objFile.hasNextDouble()) {
@@ -106,15 +100,80 @@ public class Matriks {
         }
     }
 
-    void determinanMatriks() {
-        System.out.println(this.m_baris);
+    public boolean validDeterminan() {
+        return this.m_baris == this.n_kolom;
+    }
+
+    public double determinanMatriks() {
+        if (validDeterminan()) {
+            return hitungDeterminan(this.matriks, this.m_baris);
+        } else {
+            System.out.println("Determinan matriks hanya dapat dihitung pada matriks persegi");
+            return 0;
+        }
+    }
+
+    public double hitungDeterminan(double[][] matriks, int ukuranMatriks) {
+        int i;
+
+        if (ukuranMatriks == 1) {
+            return matriks[0][0];
+        } else if (ukuranMatriks == 2) {
+            return (matriks[0][0] * matriks[1][1]) - (matriks[0][1] * matriks[1][0]);
+        }
+
+        double hasil = 0;
+        for (i = 0; i < ukuranMatriks; i++) {
+            hasil += Math.pow(-1, i) * matriks[0][i]
+                    * hitungDeterminan(minorKofaktor(matriks, 0, i), ukuranMatriks - 1);
+        }
+        return hasil;
+    }
+
+    private double[][] minorKofaktor(double[][] matriks, int baris, int kolom) {
+        double[][] hasil = new double[this.m_baris - 1][this.n_kolom - 1];
+        int bar = 0, kol = 0;
+
+        for (int i = 0; i < this.m_baris; i++) {
+            if (i == baris)
+                continue;
+            kol = 0;
+            for (int j = 0; j < this.n_kolom; j++) {
+                if (j == kolom)
+                    continue;
+                hasil[bar][kol] = matriks[i][j];
+                kol += 1;
+            }
+            bar += 1;
+        }
+        return hasil;
+    }
+
+    public double[][] kofaktorMatriks() {
+        if (!validDeterminan()) {
+            System.out.println("Kofaktor matriks hanya dapat dihitung pada matriks persegi.");
+            return null;
+        }
+
+        else {
+            int i, j;
+            double[][] kofaktor = new double[this.m_baris][this.n_kolom];
+
+            for (i = 0; i < this.m_baris; i++) {
+                for (j = 0; j < this.n_kolom; j++) {
+                    double minor = hitungDeterminan(minorKofaktor(this.matriks, i, j), this.m_baris - 1);
+                    kofaktor[i][j] = Math.pow(i, j) * minor;
+                }
+            }
+            return kofaktor;
+        }
+
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Matriks matriks = new Matriks();
 
-        // User menu to choose input method
         System.out.println("Pilih metode input:");
         System.out.println("1. Input manual");
         System.out.println("2. Baca dari file");
@@ -122,24 +181,24 @@ public class Matriks {
         int pilihan = input.nextInt();
 
         if (pilihan == 1) {
-            // Input matrix manually
             matriks.bacaMatriks();
         } else if (pilihan == 2) {
-            // Read matrix from file
             System.out.print("Masukkan nama file (misal: matrix.txt): ");
             String fileName = input.next();
             matriks.bacaMatriksDariFile(fileName);
         } else {
             System.out.println("Pilihan tidak valid!");
             input.close();
-            return; // Exit the program
+            return;
         }
 
-        // Print the matrix
         System.out.println("Matriks yang dibaca:");
         matriks.cetakMatriks();
 
-        matriks.determinanMatriks();
+        double det = matriks.determinanMatriks();
+        System.out.println("Determinan matriks yang terhitung adalah: " + det);
+
         input.close();
     }
+
 }
