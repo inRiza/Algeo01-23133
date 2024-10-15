9bbe2266549bbb9c54c8d3ab187d6dc8d51ac05f
rlukassa <19623069@std.stei.itb.ac.id>
2024-10-16 01:41:53 +0700

    lukas ngecommit lagi cuy

--- src/Gaussjordan.java
+++ src/Gaussjordan.java
@@ -1,3 +1,22 @@
 public class Gaussjordan {
-    
+    public static void gaussJordanElimination(double[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            double diagElement = matrix[i][i];
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] /= diagElement;
+            }
+
+            for (int k = 0; k < rows; k++) {
+                if (k != i) {
+                    double factor = matrix[k][i];
+                    for (int j = 0; j < cols; j++) {
+                        matrix[k][j] -= factor * matrix[i][j];
+                    }
+                }
+            }
+        }
+    }
 }
